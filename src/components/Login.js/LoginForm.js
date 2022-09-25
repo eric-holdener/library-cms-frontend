0c7bc1c02cfc5229f1544490fe827352b50020b4
EHoldener <eholdener12@gmail.com>
2022-09-25 14:34:25 -0700

    login requests are being made correctly, api not returning correct response

--- src/components/Login.js/LoginForm.js
+++ src/components/Login.js/LoginForm.js
@@ -1,7 +1,41 @@
+import axios from "axios";
+import { useState } from "react"
+
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit() {
+    axios.post("api/users/login", {
+      user: {
+        email: email,
+        password: password
+      }
+    })
+      .then(res => console.log(res))
+      .catch(error => console.log(error))
+  }
+
+  function logger() {
+    console.log(email);
+    console.log(password);
+  }
+
   return (
-    <div>
-      <p>Login Form</p>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Log in" />
+      </form>
+      <button onClick={logger}>Console.log</button>
+    </>
+
   )
 }
